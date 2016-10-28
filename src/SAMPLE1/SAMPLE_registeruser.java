ddd51b95b2716e4ca5b1ceca8b6019bacbe77d1c
ConorMoroney <moroneyboy@gmail.com>
2016-10-28 17:54:46 +0100

    Added more Skeleton code for sample package

---
+++ src/SAMPLE1/SAMPLE_registeruser.java
@@ -0,0 +1,7 @@
+package SAMPLE1;
+
+/**
+ * Created by Conor on 28-Oct-16.
+ */
+public class SAMPLE_registeruser {
+}
---
+++ src/SAMPLE1/Sample1_registerproduct_.java
@@ -0,0 +1,7 @@
+package SAMPLE1;
+
+/**
+ * Created by Conor on 28-Oct-16.
+ */
+public class Sample1_registerproduct_ {
+}

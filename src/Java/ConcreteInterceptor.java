e9949a6356ddc2466bf2e53226ca4b884dfbc7d8
cmoore94 <colmmoore1994@gmail.com>
2016-11-06 17:40:47 +0000

    Colm: Adding logging to log file from Interceptor. Need to look at this more with Conor to figure some stuff out.

--- src/Java/ConcreteInterceptor.java
+++ src/Java/ConcreteInterceptor.java
@@ -2,6 +2,11 @@ package Java;
 
 import java.awt.event.ActionEvent;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+
 /**
  * Created by Conor on 28-Oct-16.
  */
@@ -11,22 +16,29 @@ public class ConcreteInterceptor  implements I_Interceptor{
     @Override
     public void InterceptorMethod1(Context context) {
         /* Put implementation here */
-      ActionEvent event = context.getValue();
+        ActionEvent event = context.getValue();
 
         String output = "The " + event.getActionCommand() + " Button was pressed";
         //System.out.println(output);
         //"[TIME] User: Action"
         user = context.consumeService();
 
-       // JOptionPane.showMessageDialog(null, user);
+        // JOptionPane.showMessageDialog(null, user);
+        try{
+            addLogToFile(event.getActionCommand());
+            addLogToDB(event.getActionCommand());
 
-        addLogToFile();
-        addLogToDB();
-
-        if(!compareFileToDB())
-        {
-           // JOptionPane.showMessageDialog(null, "An error has occurred. Please notify an administrator.");
+            if(!compareFileToDB())
+            {
+                System.out.println("Local log file did not match database.");
+            }
         }
+        catch (Exception e)
+        {
+            System.out.println("An IO error occurred in Concrete Interceptor");
+        }
+
+
     }
     /**TODO
      * Instead of Printing to the command line
@@ -39,18 +51,25 @@ public class ConcreteInterceptor  implements I_Interceptor{
      *
      * finally if correct continue onto normal functionality */
 
-    private void addLogToFile()
+    private void addLogToFile(String action) throws IOException
     {
+        String path = "CS4227Log1.txt";
+        boolean appendToFile = true;
 
+        FileWriter write = new FileWriter(path, appendToFile);
+        PrintWriter print_line = new PrintWriter(write);
+
+        print_line.printf("[%s] %s;%s\n", user, action, System.currentTimeMillis());
+        print_line.close();
     }
 
-    private void addLogToDB()
+    private void addLogToDB(String action) throws IOException
     {
+        //SQL.Insert();
 
     }
     private boolean compareFileToDB()
     {
-
         return false;
     }
 }
